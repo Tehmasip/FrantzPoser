9a20ce223747742005854c31f726ac9de6ed45f2
Tehmasip <tehmasipkhan910@gmail.com>
2020-10-30 13:15:52 +0500

    Made New Project

---
+++ Poser/Assets/Scripts/HUD/HUDScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDScript : MonoBehaviour
+{
+    public void SettingButtonListeners()
+    {
+
+    }
+}
