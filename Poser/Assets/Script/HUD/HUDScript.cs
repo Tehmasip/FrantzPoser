94acfc646bc0d4c1848344710472db2c4288afa7
bogra202 <60652513+bogra202@users.noreply.github.com>
2020-11-01 01:28:04 -0400

    more buttons added

---
+++ Poser/Assets/Script/HUD/HUDScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SettingButtonListeners()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
