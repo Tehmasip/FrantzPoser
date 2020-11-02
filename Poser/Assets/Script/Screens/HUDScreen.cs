1032251754fdb92db6fd66fbeea50ebd1ae1d612
Tehmasip <tehmasipkhan910@gmail.com>
2020-11-03 00:20:52 +0500

    Scripting Finctions of HUD buttons

---
+++ Poser/Assets/Script/Screens/HUDScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDScreen : MonoBehaviour
+{
+    public void SettingsButton()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void SaveScreen()
+    {
+        Debug.Log("Save Screen");
+    }
+}
