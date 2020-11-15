e74f08f63a7ed1504f45c0cdc3cc197e6d1fac1d
Tehmasip <tehmasipkhan910@gmail.com>
2020-11-15 17:56:35 +0500

    Joints Module

---
+++ Poser/Assets/JointsMotion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointsMotion : MonoBehaviour
+{
+    public static JointsMotion jointsMotion;
+
+    private void Awake()
+    {
+        if(jointsMotion == null)
+        {
+            jointsMotion = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Poser/Assets/SelectJoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectJoint : MonoBehaviour
+{
+    private void OnMouseDown()//it is called when ever we click or touch the 
+    {
+        RotateWithDrag.Instance.SelectedJoint = this.transform;
+    }
+}
